d8caf456e543b63a64835bd387308b50693c42fb
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-04-16 11:33:12 -0700

    - ui

---
+++ MotivationNotification/BundleDecoding.swift
@@ -0,0 +1,9 @@
+//
+//  BundleDecoding.swift
+//  MotivationNotification
+//
+//  Created by Prudhvi Gadiraju on 4/16/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import Foundation
--- MotivationNotification/ViewController.swift
+++ MotivationNotification/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var quote: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
